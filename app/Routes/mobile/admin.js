7d581261afa2dd689f318c7270dc35a6c47c1ff8
singh150 <prashant2.fourbrick@gmail.com>
2021-08-08 19:10:43 +0530

    prashant

--- app/Routes/mobile/admin.js
+++ app/Routes/mobile/admin.js
@@ -1,32 +1,34 @@
-const multer = require("../../middlewares/multer");
+const multer = require('../../middlewares/multer');
 
-const express = require("express"),
+const express = require('express'),
   Route = express.Router(),
-  Controller = require("../../Controller/mobile");
+  Controller = require('../../Controller/mobile');
 
-Route.post("/verifyStudent", Controller.Admin.verifyStudent);
-Route.post("/login", Controller.Admin.login);
-Route.post("/updateStudent/:id", Controller.Admin.updateStudent);
-Route.post("/verifyEvent", Controller.Admin.verifyEvent);
+Route.post('/verifyStudent', Controller.Admin.verifyStudent);
+Route.post('/login', Controller.Admin.login);
+Route.post('/updateStudent/:id', Controller.Admin.updateStudent);
+Route.post('/verifyEvent', Controller.Admin.verifyEvent);
 
-Route.post("/updateStudent/:id", Controller.Admin.updateStudent);
-Route.get("/getAllStudent", Controller.Admin.getAllStudent);
-Route.get("/getOneStudent", Controller.Admin.getOneStudent);
-Route.get("/getAllAlumini", Controller.Admin.getAllAlumini);
 
-Route.delete("/deleteMagzines/:magzineId", Controller.Admin.deleteMagzines);
+
+Route.post('/addStudent', Controller.Admin.addStudent);
+Route.post('/updateStudent/:id', Controller.Admin.updateStudent);
+Route.get('/getAllStudent', Controller.Admin.getAllStudent);
+Route.get('/getOneStudent', Controller.Admin.getOneStudent);
+Route.get('/getAllAlumini', Controller.Admin.getAllAlumini);
+
+Route.delete('/deleteMagzines/:magzineId', Controller.Admin.deleteMagzines);
 Route.post(
-  "/addAnnouncement",
-  multer.upload.single("image"),
+  '/addAnnouncement',
+  multer.upload.single('image'),
   Controller.Admin.addAnnouncement
 );
-Route.get("/getAnnouncement", Controller.Admin.getAnnouncement);
-Route.delete("/deleteAnnouncement/:id", Controller.Admin.deleteAnnouncement);
+Route.get('/getAnnouncement', Controller.Admin.getAnnouncement);
+Route.delete('/deleteAnnouncement/:id', Controller.Admin.deleteAnnouncement);
 
-Route.get("/getAllSuggestion", Controller.Admin.getAllSuggestion);
-Route.get("/getAllVerifiedStudent", Controller.Admin.getAllVerifiedStudent);
-
-
+Route.get('/getAllSuggestion', Controller.Admin.getAllSuggestion);
+Route.get('/getAllVerifiedStudent', Controller.Admin.getAllVerifiedStudent);
+Route.delete('/deleteStudent/:id', Controller.Admin.deleteStudent);
 
 
 
